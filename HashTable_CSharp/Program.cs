09a246ad0eaf8a7c20e23dc2b3b5a20d9c8610e0
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-17 20:03:39 +0530

    [NIJAM] Add:Find Frequency of Word in sentense.

--- HashTable_CSharp/Program.cs
+++ HashTable_CSharp/Program.cs
@@ -7,6 +7,21 @@ namespace HashTable_CSharp
         static void Main(string[] args)
         {
             Console.WriteLine("***HashTable_CSharp***");
-        }
+			MyMapNode<String, int> myHashMap = new MyMapNode<String, int>(5);
+			String sentence = "to be or not to be";
+			String[] words = sentence.ToLower().Split(" ");
+			foreach (String word in words)
+			{
+				Console.WriteLine(word + " -->Added to HashTable");
+				int value = myHashMap.Get(word);
+				if (value == 0)
+					value = 1;
+				else
+					value = value + 1;
+				myHashMap.Add(word, value);
+			}
+			int frequency = myHashMap.Get("be");
+			Console.WriteLine("Frequency of word -> be -> " + frequency);
+		}
     }
 }
