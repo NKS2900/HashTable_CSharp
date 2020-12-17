538fcb51513b920388b8a930cc5fde09cff10a81
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-17 20:13:19 +0530

    [NIJAM] Add:Finding Frequency of word in large para.

--- HashTable_CSharp/Program.cs
+++ HashTable_CSharp/Program.cs
@@ -22,6 +22,24 @@ namespace HashTable_CSharp
 			}
 			int frequency = myHashMap.Get("be");
 			Console.WriteLine("Frequency of word -> be -> " + frequency);
-		}
+			Console.WriteLine("--------------------------------------");
+            sentence = "Paranoids are not paranoid because they are paranoid but because they " +
+                "keep putting themselves deliberately into paranoid avoidable situations";
+            words = sentence.ToLower().Split(" ");
+            foreach (String word in words)
+            {
+                Console.WriteLine(word + " -->Added to HashTable");
+                int value = myHashMap.Get(word);
+                if (value == 0)
+                    value = 1;
+                else
+                    value = value + 1;
+                myHashMap.Add(word, value);
+            }
+            frequency = myHashMap.Get("paranoid");
+            Console.WriteLine("Frequency of word -->paranoid --> " + frequency);
+            Console.ReadLine();
+          
+        }
     }
 }
